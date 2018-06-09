c0fe5def0172790d5aabeb66c19896ae4f944022
Giovani <giovanibossoni@hotmail.com>
2018-06-09 16:27:04 -0300

    correcao

---
+++ Client/rest.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type tipo struct {
+	id
+	data
+	origem
+	destino
+	numero
+	tipo
+}
+
+type
+
+
+func main() {
+	url := "http://localhost:8080"
+	resposta, erro := http.Get(url)
+	if erro != nil {
+		panic(erro)
+	}
+	defer resposta.Body.Close()
+
+	body, err := ioutil.ReadAll(resposta.Body)
+	if erro != nil {
+		panic(erro)
+	}
+	var p Payload
+
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		panic(err)
+	}
+
+}
